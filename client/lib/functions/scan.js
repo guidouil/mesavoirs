36017b62d18a03b5bf20f1172d2383454e51fd57
Guidouil <guidouil@gmail.com>
2015-12-03 23:13:30 +0100

    scan fix

--- client/lib/functions/scan.js
+++ client/lib/functions/scan.js
@@ -6,6 +6,7 @@ scan = function () {
           if (result.text.search('userId:') === 0 || result.text.search('placeId:') === 0) {
             var scanned = result.text.split(':');
             Router.go('scanned', {type: scanned[0], id: scanned[1]});
+            return;
           }
         }
         Router.go('scanned', {type: result.format, id: result.text});
