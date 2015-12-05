3020c433dcfff34b4a7e5730728708f9638fc923
Guidouil <guidouil@gmail.com>
2015-12-05 01:05:53 +0100

    ean_13 bug

--- client/templates/card/card_script.js
+++ client/templates/card/card_script.js
@@ -1,18 +1,6 @@
 Template.card.helpers({
   isQrCode: function () {
     return this.format === 'QR_CODE';
-  },
-  reFormat: function (format) {
-    var codeFormats = {
-      'CODABAR': 'Codabar',
-      'CODE_128': 'Code 128',
-      'CODE_39': 'Code 39',
-      'EAN_13': 'EAN-13',
-      'EAN_8': 'EAN-8',
-      'UPC_A': 'UPC-A',
-      'ITF': 'ITF'
-    };
-    return codeFormats[format] || format;
   }
 });
 
@@ -35,6 +23,9 @@ Template.card.onRendered(function () {
     var options = {};
     options.type = format;
     var g = canvas.getContext('2d');
+    if (this.data.format === 'EAN_13') {
+      this.data.code = this.data.code.slice(0, -1);
+    }
     drawBarcode(g, this.data.code, options);
   }
 });
