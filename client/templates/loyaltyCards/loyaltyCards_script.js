12cc112eaf6309b0d4d11397ca3c87aba7050efc
Guidouil <guidouil@gmail.com>
2015-12-09 10:50:24 +0100

    reformat lists

--- client/templates/loyaltyCards/loyaltyCards_script.js
+++ client/templates/loyaltyCards/loyaltyCards_script.js
@@ -9,6 +9,19 @@ Template.loyaltyCards.helpers({
     if (Session.equals('loyaltyTab', tabName)) {
       return 'active';
     }
+  },
+  reFormat: function (format) {
+    var codeFormats = {
+      'QR_CODE': 'QR Code',
+      'CODABAR': 'Codabar',
+      'CODE_128': 'Code 128',
+      'CODE_39': 'Code 39',
+      'EAN_13': 'EAN-13',
+      'EAN_8': 'EAN-8',
+      'UPC_A': 'UPC-A',
+      'ITF': 'ITF'
+    };
+    return codeFormats[format] || format;
   }
 });
 
