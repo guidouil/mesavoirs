13a0bcbb6f5fec6343c9993ca5a4bc3ec759f759
Guidouil <guidouil@gmail.com>
2015-12-25 23:44:59 +0100

    fix setDefaultCurrentPlace

--- client/lib/functions/setDefaultCurrentPlace.js
+++ client/lib/functions/setDefaultCurrentPlace.js
@@ -1,11 +1,7 @@
 setDefaultCurrentPlace = function () {
   var user = Meteor.user();
   var myPlace = Places.findOne({ $or: [{owners: Meteor.userId()}, {sellers: Meteor.userId() }] });
-  if ((!user.profile || !user.profile.currentPlace) && myPlace) {
+  if (user && myPlace && (!user.profile || !user.profile.currentPlace)) {
     Meteor.users.update( { _id: Meteor.userId() }, { $set: { 'profile.currentPlace': myPlace._id }});
-  }
-  // clean forgoten roles
-  if (!myPlace && Roles.userIsInRole(Meteor.userId(), ['sellers', 'owners'])) {
-
   }
 };
