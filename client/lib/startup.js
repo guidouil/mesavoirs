359e3e455c80c6e6f74be30313a31d558bee9c48
Guidouil <guidouil@gmail.com>
2016-01-12 11:03:34 +0100

    better ground:db

--- client/lib/startup.js
+++ client/lib/startup.js
@@ -1,3 +1,12 @@
 Meteor.startup(function () {
   AutoForm.setDefaultTemplate('semanticUI');
+
+  // Ground DB
+  Ground.Collection(Meteor.users);
+  Ground.Collection(CardsBrands);
+  Ground.Collection(Growls);
+  Ground.Collection(LoyaltyCards);
+  Ground.Collection(Places);
+  Ground.Collection(PrivateLoyaltyCards);
+  Ground.Collection(Vouchers);
 });
