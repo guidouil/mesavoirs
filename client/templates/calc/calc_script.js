dde73e644c5cf887b9be6fc04081eebcf343036f
Guidouil <guidouil@gmail.com>
2016-01-16 00:38:48 +0100

    42

--- client/templates/calc/calc_script.js
+++ client/templates/calc/calc_script.js
@@ -1,6 +1,9 @@
 Template.calc.helpers({
   calcResult: function () {
     return Session.get('calcResult');
+  },
+  voucher: function () {
+    return Session.get('voucher') || 0;
   }
 });
 
@@ -11,11 +14,15 @@ Template.calc.events({
   },
   'click #equal': function (evt, tmpl) {
     var calcScreen = tmpl.find('#calcScreen').value;
-    var re = /\d/;
-    if (re.test(calcScreen.charAt(0)) && re.test(calcScreen.charAt(calcScreen.length - 1))) {
+    var re2 = /\d|\./;
+    if (re2.test(calcScreen.charAt(0)) && re2.test(calcScreen.charAt(calcScreen.length - 1))) {
       tmpl.find('#calcScreen').value = eval(calcScreen);
-      Session.set('calcResult', tmpl.find('#calcScreen').value);
     }
+    var re1 = /\/0+$/;
+    if (re1.test(calcScreen)) {
+      tmpl.find('#calcScreen').value = 42;
+    }
+    Session.set('calcResult', tmpl.find('#calcScreen').value);
   },
   'click #cancel': function (evt, tmpl) {
     Session.delete('calcResult');
@@ -24,10 +31,26 @@ Template.calc.events({
   'click #toVoucher': function () {
     Session.set('voucher', Session.get('calcResult'));
     history.go(-1);
+  },
+  'focus input, blur input, change select, click .calcVoucher': function (evt, tmpl) {
+    var total = parseFloat(tmpl.find('#total').value);
+    var ticketValue = parseFloat(tmpl.find('#ticketValue').value);
+    var ticketCount = parseInt(tmpl.find('#ticketCount').value);
+    check(total, Number);
+    check(ticketValue, Number);
+    check(ticketCount, Number);
+    var voucher =  ticketValue * ticketCount - total;
+    if (voucher >= 0) {
+      Session.set('voucher', voucher);
+    }
   }
 });
 
 Template.calc.onCreated(function () {
   Session.delete('calcResult');
   Session.delete('voucher');
+});
+
+Template.calc.onRendered(function () {
+  $('#calcTabs .item').tab();
 });
