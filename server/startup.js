cf3383575bff8e9b7d56f278b44ae874c00630bc
Guidouil <guidouil@gmail.com>
2016-01-17 01:33:19 +0100

    fix mail bug

--- server/startup.js
+++ server/startup.js
@@ -3,5 +3,8 @@ if (Meteor.isServer) {
     // Houstan admin view collections users and _admins
     Houston.add_collection(Meteor.users);
     Houston.add_collection(Houston._admins);
+
+    Accounts.emailTemplates.from = 'Loyali <no-reply@loya.li>';
+    Accounts.emailTemplates.siteName = 'Loyali';
   });
 }
