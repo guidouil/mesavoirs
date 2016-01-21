2522d2595cca54d9e8b4ec77d0c4d1721858079b
Guidouil <guidouil@gmail.com>
2016-01-21 15:40:03 +0100

    quicky

--- mobile-config.js
+++ mobile-config.js
@@ -5,7 +5,7 @@ App.info({
   author: 'guillaume.darbonne@gmail.com',
   email: 'guillaume.darbonne@gmail.com',
   website: 'http://loya.li',
-  version: '1.1.7'
+  version: '1.1.8'
 });
 
 App.icons({
