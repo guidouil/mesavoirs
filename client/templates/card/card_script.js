78fb86ff1922a71c7020df40349424155c4ecbf6
Guidouil <guidouil@gmail.com>
2016-02-01 23:03:47 +0100

    Remove barcode flickering

--- client/templates/card/card_script.js
+++ client/templates/card/card_script.js
@@ -9,33 +9,31 @@ Template.card.helpers({
 
 Template.card.onRendered(function () {
   var template = this;
-  setTimeout(function () {
-    var canvas = template.find('#barcodeCanvas');
-    if (canvas) {
-      var card = PrivateLoyaltyCards.findOne({_id: Router.current().params.cardId});
-      if (card && card.format !== 'QR_CODE') {
-        var codeFormats = {
-          'CODABAR': 'Codabar',
-          'CODE_128': 'Code 128',
-          'CODE_39': 'Code 39',
-          'EAN_13': 'EAN-13',
-          'EAN_8': 'EAN-8',
-          'UPC_A': 'UPC-A',
-          'ITF': 'ITF'
-        };
-        var format = codeFormats[card.format] || card.format; // for uknown formats
-        var options = {};
-        options.type = format;
-        options.height = 100;
-        options.maxWidth = 300;
-        var g = canvas.getContext('2d');
-        if (card.format === 'EAN_8' || card.format === 'EAN_13' || card.format === 'UPC_A') {
-          card.code = card.code.slice(0, -1); // removing EAN or UPC validation bit
-        }
-        drawBarcode(g, card.code, options);
+  var canvas = template.find('#barcodeCanvas');
+  if (canvas) {
+    var card = PrivateLoyaltyCards.findOne({_id: Router.current().params.cardId});
+    if (card && card.format !== 'QR_CODE') {
+      var codeFormats = {
+        'CODABAR': 'Codabar',
+        'CODE_128': 'Code 128',
+        'CODE_39': 'Code 39',
+        'EAN_13': 'EAN-13',
+        'EAN_8': 'EAN-8',
+        'UPC_A': 'UPC-A',
+        'ITF': 'ITF'
+      };
+      var format = codeFormats[card.format] || card.format; // for uknown formats
+      var options = {};
+      options.type = format;
+      options.height = 100;
+      options.maxWidth = 300;
+      var g = canvas.getContext('2d');
+      if (card.format === 'EAN_8' || card.format === 'EAN_13' || card.format === 'UPC_A') {
+        card.code = card.code.slice(0, -1); // removing EAN or UPC validation bit
       }
+      drawBarcode(g, card.code, options);
     }
-  }, 700);
+  }
 });
 
 Template.card.onCreated(function () {
