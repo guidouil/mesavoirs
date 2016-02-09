e7a6d4bd8dbd169d4969702bc8c75c55325508de
Guidouil <guidouil@gmail.com>
2016-02-09 09:21:09 +0100

    Back to GridFS temporarely

--- client/templates/image/image_script.js
+++ client/templates/image/image_script.js
@@ -1,8 +1,19 @@
 Template.image.helpers({
   imageUrl: function () {
     if (this.imageId) {
-      return Meteor.absoluteUrl() + 'uploads/compressed/images-' + this.imageId + '-loyali.png';
+      var image = Images.findOne({_id: this.imageId});
+      if (image && image.isUploaded()) {
+        return Meteor.absoluteUrl() + 'cfs/files/images/' + this.imageId;
+      }
+      // return Meteor.absoluteUrl() + 'uploads/compressed/images-' + this.imageId + '-loyali.png';
     }
     return Meteor.absoluteUrl() + 'default-image.png';
+  }
+});
+
+Template.image.onCreated(function () {
+  var template = this;
+  if (template.data.imageId) {
+    subs.subscribe('Image', template.data.imageId);
   }
 });
