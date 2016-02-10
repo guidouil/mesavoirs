98872146acdda7e17ce3b934ea79de6714f4887c
Guidouil <guidouil@gmail.com>
2016-02-10 15:09:51 +0100

    Compressed pmgs

--- client/templates/image/image_script.js
+++ client/templates/image/image_script.js
@@ -1,19 +1,8 @@
 Template.image.helpers({
   imageUrl: function () {
     if (this.imageId) {
-      var image = Images.findOne({_id: this.imageId});
-      if (image && image.isUploaded()) {
-        return Meteor.absoluteUrl() + 'cfs/files/images/' + this.imageId;
-      }
-      // return Meteor.absoluteUrl() + 'uploads/compressed/images-' + this.imageId + '-loyali.png';
+      return Meteor.absoluteUrl() + 'compressed/images-' + this.imageId + '-loyali.png';
     }
     return Meteor.absoluteUrl() + 'default-image.png';
-  }
-});
-
-Template.image.onCreated(function () {
-  var template = this;
-  if (template.data.imageId) {
-    subs.subscribe('Image', template.data.imageId);
   }
 });
